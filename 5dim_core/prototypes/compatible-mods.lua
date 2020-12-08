2a6c5b101c5d1a0e5376baebea888463e08dc0a6
McGuten <mcguten.videos@gmail.com>
2020-12-08 14:40:24 +0100

    aai industry compatibility

--- 5dim_core/prototypes/compatible-mods.lua
+++ 5dim_core/prototypes/compatible-mods.lua
@@ -5,3 +5,15 @@ end
 if mods["LoaderRedux"] and mods["5dim_transport"] then
     data.raw["loader"]["express-loader"].next_upgrade = "5d-loader-04"
 end
+if mods["aai-industry"] then
+    data.raw.recipe["power-armor-mk2"].ingredients = {
+        {"power-armor", 1},
+        {"effectivity-module-2", 25},
+        {"speed-module-2", 25},
+        {"processing-unit", 60},
+        {"electric-engine-unit", 40},
+        {"low-density-structure", 30}
+    }
+    data.raw["offshore-pump"]["offshore-pump"].collision_box = {{-0.6, -1.05}, {0.6, 0.3}}
+    data.raw["offshore-pump"]["offshore-pump"].selection_box = {{-0.6, -1.49}, {0.6, 0.49}}
+end

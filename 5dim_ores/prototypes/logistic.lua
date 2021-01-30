0548f04de0dee55556378011162ba26f6a34be82
TTvanWillegen <mail@tobyvanwillegen.nl>
2021-01-30 17:42:45 +0100

    First adaption to 1.1.0 for 5Dim Ores

--- 5dim_ores/prototypes/logistic.lua
+++ 5dim_ores/prototypes/logistic.lua
@@ -1,45 +1,29 @@
-data.raw.recipe["5d-repair-pack-2"].ingredients = {
-    {"electronic-circuit", 2},
-    {"iron-gear-wheel", 1}
-}
-data.raw.recipe["5d-passive"].ingredients = {
-    {"logistic-chest-passive-provider", 1},
-    {"advanced-circuit", 1}
-}
-data.raw.recipe["5d-storage"].ingredients = {
-    {"logistic-chest-storage", 1},
-    {"advanced-circuit", 1}
-}
-data.raw.recipe["5d-logistic-robot-2"].ingredients = {
+data.raw.recipe["5d-logistic-robot-02"].ingredients = {
     {"logistic-robot", 1},
     {"advanced-circuit", 2}
 }
-data.raw.recipe["5d-construction-robot-2"].ingredients = {
+data.raw.recipe["5d-construction-robot-02"].ingredients = {
     {"construction-robot", 1},
     {"electronic-circuit", 2}
 }
-data.raw.recipe["5d-roboport-2"].ingredients = {
+data.raw.recipe["5d-roboport-02"].ingredients = {
     {"roboport", 1},
     {"zinc-plate", 15},
     {"processing-unit", 20}
 }
-data.raw.recipe["5d-requester"].ingredients = {
-    {"logistic-chest-requester", 1},
-    {"advanced-circuit", 1}
-}
-data.raw.recipe["5d-logistic-robot-3"].ingredients = {
-    {"5d-logistic-robot-2", 1},
+data.raw.recipe["5d-logistic-robot-03"].ingredients = {
+    {"5d-logistic-robot-02", 1},
     {"advanced-circuit", 2}
 }
-data.raw.recipe["5d-construction-robot-3"].ingredients = {
-    {"5d-construction-robot-2", 1},
+data.raw.recipe["5d-construction-robot-03"].ingredients = {
+    {"5d-construction-robot-02", 1},
     {"electronic-circuit", 2}
 }
-data.raw.recipe["5d-logistic-robot-4"].ingredients = {
-    {"5d-logistic-robot-3", 1},
+data.raw.recipe["5d-logistic-robot-04"].ingredients = {
+    {"5d-logistic-robot-03", 1},
     {"advanced-circuit", 2}
 }
-data.raw.recipe["5d-construction-robot-4"].ingredients = {
-    {"5d-construction-robot-3", 1},
+data.raw.recipe["5d-construction-robot-04"].ingredients = {
+    {"5d-construction-robot-03", 1},
     {"electronic-circuit", 2}
 }
--- 5dim_ores/prototypes/mining.lua
+++ 5dim_ores/prototypes/mining.lua
@@ -1,61 +1,29 @@
-data.raw.recipe["5d-mining-drill-speed-3"].ingredients = {
-    {"5d-mining-drill-speed-2", 1},
+data.raw.recipe["5d-electric-mining-drill-04"].ingredients = {
+    {"5d-electric-mining-drill-03", 1},
     {"zinc-plate", 10},
     {"5d-gold-circuit", 5}
 }
-data.raw.recipe["5d-mining-drill-speed-2"].ingredients = {
-    {"5d-mining-drill-speed-1", 1},
+data.raw.recipe["5d-electric-mining-drill-03"].ingredients = {
+    {"5d-electric-mining-drill-02", 1},
     {"5d-zinc-gear-wheel", 5},
     {"advanced-circuit", 5}
 }
-data.raw.recipe["5d-mining-drill-speed-1"].ingredients = {
+data.raw.recipe["5d-electric-mining-drill-02"].ingredients = {
     {"electric-mining-drill", 1},
     {"lead-plate", 10},
     {"5d-tin-gear-wheel", 5},
     {"electronic-circuit", 2}
 }
-data.raw.recipe["5d-mining-drill-range-3"].ingredients = {
-    {"5d-mining-drill-speed-2", 1},
-    {"zinc-plate", 10},
-    {"5d-gold-circuit", 5}
+data.raw.recipe["5d-pumpjack-03"].ingredients = {
+	{"5d-tin-gear-wheel", 20},
+	{"lead-plate", 20},
+	{"5d-pumpjack-02", 1},
+	{"pipe", 25}
 }
-data.raw.recipe["5d-mining-drill-range-2"].ingredients = {
-    {"5d-mining-drill-speed-1", 1},
-    {"5d-zinc-gear-wheel", 5},
-    {"advanced-circuit", 5}
+data.raw.recipe["5d-pumpjack-02"].ingredients = {
+	{"tin-plate", 35},
+	{"pumpjack", 1},
+	{"5d-tin-gear-wheel", 15},
+	{"electronic-circuit", 10},
+	{"pipe", 5}
 }
-data.raw.recipe["5d-mining-drill-range-1"].ingredients = {
-    {"electric-mining-drill", 1},
-    {"lead-plate", 10},
-    {"5d-tin-gear-wheel", 5},
-    {"electronic-circuit", 2}
-}
-if transport == true then
-    data.raw.recipe["5d-pumpjack-3"].ingredients = {
-        {"5d-tin-gear-wheel", 20},
-        {"lead-plate", 20},
-        {"5d-pumpjack-2", 1},
-        {"5d-pipe-mk3", 25}
-    }
-    data.raw.recipe["5d-pumpjack-2"].ingredients = {
-        {"tin-plate", 35},
-        {"pumpjack", 1},
-        {"5d-tin-gear-wheel", 15},
-        {"electronic-circuit", 10},
-        {"5d-pipe-mk2", 5}
-    }
-else
-    data.raw.recipe["5d-pumpjack-3"].ingredients = {
-        {"5d-tin-gear-wheel", 20},
-        {"lead-plate", 20},
-        {"5d-pumpjack-2", 1},
-        {"pipe", 25}
-    }
-    data.raw.recipe["5d-pumpjack-2"].ingredients = {
-        {"tin-plate", 35},
-        {"pumpjack", 1},
-        {"5d-tin-gear-wheel", 15},
-        {"electronic-circuit", 10},
-        {"pipe", 5}
-    }
-end

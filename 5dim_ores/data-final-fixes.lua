ce2932e3a8e5e73736d7f94ebdfd617703a0f2e2
McGuten <mcguten.videos@gmail.com>
2024-11-03 16:29:03 +0100

    remove bot recaller and ores

--- 5dim_ores/data-final-fixes.lua
+++
@@ -1,5 +0,0 @@
-data.raw.item["bauxite-ore"].subgroup = "plates-ore"
-data.raw.item["gold-ore"].subgroup = "plates-ore"
-data.raw.item["lead-ore"].subgroup = "plates-ore"
-data.raw.item["tin-ore"].subgroup = "plates-ore"
-data.raw.item["zinc-ore"].subgroup = "plates-ore"

b8926d34a2996e69ef077f7773b06aa0b93b6430
McGuten <mcguten.videos@gmail.com>
2024-11-04 23:06:41 +0100

    cambio graficos chemical

--- 5dim_core/lib/automation/generation-chemical-plant.lua
+++ 5dim_core/lib/automation/generation-chemical-plant.lua
@@ -53,27 +53,27 @@ function genChemicalPlants(inputs)
     entity.graphics_set.animation.west.layers[1].filename =
         "__5dim_automation__/graphics/entities/chemical-plant/chemical-plant-" .. inputs.number .. ".png"
 
-    make_4way_animation_from_spritesheet({ layers =
-    {
-        {
-            filename = "__base__/graphics/entity/chemical-plant/chemical-plant" .. inputs.number .. ".png",
-            width = 220,
-            height = 292,
-            frame_count = 24,
-            line_length = 12,
-            shift = util.by_pixel(0.5, -9),
-            scale = 0.5
-        },
-        {
-            filename = "__base__/graphics/entity/chemical-plant/chemical-plant-shadow.png",
-            width = 312,
-            height = 222,
-            repeat_count = 24,
-            shift = util.by_pixel(27, 6),
-            draw_as_shadow = true,
-            scale = 0.5
-        }
-    }})
+    -- make_4way_animation_from_spritesheet({ layers =
+    -- {
+    --     {
+    --         filename = "__base__/graphics/entity/chemical-plant/chemical-plant" .. inputs.number .. ".png",
+    --         width = 220,
+    --         height = 292,
+    --         frame_count = 24,
+    --         line_length = 12,
+    --         shift = util.by_pixel(0.5, -9),
+    --         scale = 0.5
+    --     },
+    --     {
+    --         filename = "__base__/graphics/entity/chemical-plant/chemical-plant-shadow.png",
+    --         width = 312,
+    --         height = 222,
+    --         repeat_count = 24,
+    --         shift = util.by_pixel(27, 6),
+    --         draw_as_shadow = true,
+    --         scale = 0.5
+    --     }
+    -- }})
 
     data:extend({entity, recipe, item})
 

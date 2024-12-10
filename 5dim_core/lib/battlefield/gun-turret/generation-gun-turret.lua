48d1e3c4b7ad598c00dd92dd4fc89997c0ab4f04
McGuten <mcguten.videos@gmail.com>
2024-12-10 15:31:47 +0100

    update

--- 5dim_core/lib/battlefield/gun-turret/generation-gun-turret.lua
+++ 5dim_core/lib/battlefield/gun-turret/generation-gun-turret.lua
@@ -113,38 +113,51 @@ function genGunTurrets(inputs)
         }
         data:extend({ tech })
     end
-    table.insert(
-        data.raw.technology["physical-projectile-damage-1"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.1 }
-    )
+    if inputs.new then
+        table.insert(
+            data.raw.technology["physical-projectile-damage-1"].effects,
+            { type = "turret-attack", turret_id = item.name, modifier = 0.1 }
+        )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-2"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.1 }
-    )
+        table.insert(
+            data.raw.technology["physical-projectile-damage-2"].effects,
+            { type = "turret-attack", turret_id = item.name, modifier = 0.1 }
+        )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-3"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
-    )
+        table.insert(
+            data.raw.technology["physical-projectile-damage-3"].effects,
+            { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
+        )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-4"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
-    )
+        table.insert(
+            data.raw.technology["physical-projectile-damage-4"].effects,
+            { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
+        )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-5"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
-    )
+        table.insert(
+            data.raw.technology["physical-projectile-damage-5"].effects,
+            { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
+        )
+        if mods['space-age'] then
+            table.insert(
+                data.raw.technology["physical-projectile-damage-6"].effects,
+                { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
+            )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-6"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.4 }
-    )
+            table.insert(
+                data.raw.technology["physical-projectile-damage-7"].effects,
+                { type = "turret-attack", turret_id = item.name, modifier = 0.2 }
+            )
+        else
+            table.insert(
+                data.raw.technology["physical-projectile-damage-6"].effects,
+                { type = "turret-attack", turret_id = item.name, modifier = 0.4 }
+            )
 
-    table.insert(
-        data.raw.technology["physical-projectile-damage-7"].effects,
-        { type = "turret-attack", turret_id = item.name, modifier = 0.7 }
-    )
+            table.insert(
+                data.raw.technology["physical-projectile-damage-7"].effects,
+                { type = "turret-attack", turret_id = item.name, modifier = 0.7 }
+            )
+        end
+    end
 end

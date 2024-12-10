dbade3b674ddcea4ee5544260f2579123ba1c14f
McGuten <mcguten.videos@gmail.com>
2024-12-10 15:57:21 +0100

    massive update for compatibility

--- 5dim_transport/data-updates.lua
+++ 5dim_transport/data-updates.lua
@@ -1,3 +1,21 @@
+-- Pipe Ground 30
+require("prototypes.pipe-ground-1-30")
+
+-- Pipe Ground 50
+require("prototypes.pipe-ground-1-50")
+
+-- Transport belt
+require("prototypes.gen-transport-belt")
+
+-- Inserters
+require("prototypes.gen-inserter")
+
+--small pump
+require("prototypes.gen-pump")
+
+--Changes
+require("prototypes.changes")
+
 if mods['space-age'] then
     -- Stack inserters
     require("prototypes.gen-stack-inserter")
